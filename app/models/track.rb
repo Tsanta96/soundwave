08cc5bb3338fc590b6cd4b05b7a620af1eff3e08
Matteo Santavicca <matteosantavicca12@gmail.com>
2019-12-26 14:26:27 -0500

    Added active storage validations

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,9 @@
 class Track < ApplicationRecord
     validates :title, presence: true 
     validates :artist_id, presence: true
+
+    validate :ensure_track_file
+    # validate :ensure_img_file
     
     belongs_to :artist,
     foreign_key: :artist_id,
@@ -8,4 +11,16 @@ class Track < ApplicationRecord
 
     has_one_attached :track_file
     has_one_attached :img_file
+
+    def ensure_track_file 
+        unless self.track_file.attached?
+            errors[:track_file] << "Must be attached"
+        end
+    end
+
+    # def ensure_img_file 
+    #     unless self.img_file.attached?
+    #         errors[:img_file] << "Must be attached"
+    #     end
+    # end
 end
