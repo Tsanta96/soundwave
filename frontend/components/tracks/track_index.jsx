f1c4825f50d0e5e6f954d4be98c6b81ccc5c2dbc
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-27 23:51:38 -0400

    Refactor TrackIndex Component to use hooks

--- frontend/components/tracks/track_index.jsx
+++ frontend/components/tracks/track_index.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TrackItemContainer from './track_item_container';
 
-class TrackIndex extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const TrackIndex = (props) => {
+    useEffect(() => {
+        props.fetchTracks();
+    }, []);
 
-    componentDidMount() {
-        this.props.fetchTracks();
-    }
-
-    render() {
-        return (
-            <div className="discover-tracks">
-                <h1>Discover</h1>
-                {this.props.tracks.map((track, idx) => <TrackItemContainer key={track.id} idx={idx} track={track}/>)}
-                <div id="sound-wave-everywhere"><img id="sound-wave-everywhere" src="https://d313rqwfqaf3f.cloudfront.net/design/wave_white.svg" width="400" height="400"></img></div>
-            </div>
-        )
-    }
+    return (
+        <div className="discover-tracks">
+            <h1>Discover</h1>
+            {props.tracks.map((track, idx) => <TrackItemContainer key={track.id} idx={idx} track={track} />)}
+            <div id="sound-wave-everywhere"><img id="sound-wave-everywhere" src="https://d313rqwfqaf3f.cloudfront.net/design/wave_white.svg" width="400" height="400"></img></div>
+        </div>
+    )
 }
 
 export default TrackIndex;
+
+// class TrackIndex extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     componentDidMount() {
+//         this.props.fetchTracks();
+//     }
+
+//     render() {
+//         return (
+//             <div className="discover-tracks">
+//                 <h1>Discover</h1>
+//                 {this.props.tracks.map((track, idx) => <TrackItemContainer key={track.id} idx={idx} track={track}/>)}
+//                 <div id="sound-wave-everywhere"><img id="sound-wave-everywhere" src="https://d313rqwfqaf3f.cloudfront.net/design/wave_white.svg" width="400" height="400"></img></div>
+//             </div>
+//         )
+//     }
+// }
