a6b0eb83b4195883cb54d1ee86af84b92607fdf6
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-27 22:25:10 -0400

    refactor library to functional component with hooks

--- frontend/components/library/library.jsx
+++ frontend/components/library/library.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import TrackItemContainer from '../tracks/track_item_container';
 
-export class library extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const Library = props => {
+    useEffect(() => {
+        props.fetchLikedTracks(props.currentUser.id);
+    }, []);
 
-    componentDidMount() {
-        this.props.fetchLikedTracks(this.props.currentUser.id);
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="library-tracks">
-                    <h1>Library</h1>
-                    <p className="library-note">These are tracks that you've liked. You can remove them by unliking them</p>
-                    {this.props.tracks.map((track, idx) => <TrackItemContainer key={track.id} idx={idx} track={track} />)}
-                    <div id="sound-wave-everywhere"><img id="sound-wave-everywhere" src="https://d313rqwfqaf3f.cloudfront.net/design/wave_white.svg" width="400" height="400"></img></div>
-                </div>
+    return (
+        <div>
+            <div className="library-tracks">
+                <h1>Library</h1>
+                <p className="library-note">These are tracks that you've liked. You can remove them by unliking them</p>
+                {props.tracks.map((track, idx) => <TrackItemContainer key={track.id} idx={idx} track={track} />)}
+                <div id="sound-wave-everywhere"><img id="sound-wave-everywhere" src="https://d313rqwfqaf3f.cloudfront.net/design/wave_white.svg" width="400" height="400"></img></div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
-export default library
+// export class library extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     componentDidMount() {
+//         this.props.fetchLikedTracks(this.props.currentUser.id);
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <div className="library-tracks">
+//                     <h1>Library</h1>
+//                     <p className="library-note">These are tracks that you've liked. You can remove them by unliking them</p>
+//                     {this.props.tracks.map((track, idx) => <TrackItemContainer key={track.id} idx={idx} track={track} />)}
+//                     <div id="sound-wave-everywhere"><img id="sound-wave-everywhere" src="https://d313rqwfqaf3f.cloudfront.net/design/wave_white.svg" width="400" height="400"></img></div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+export default Library;
