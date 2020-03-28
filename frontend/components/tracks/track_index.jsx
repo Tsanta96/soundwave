f8ac6b8406c45f79c1ca51c15ce1076176fdb740
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-03-28 00:28:26 -0400

    Remove pre exesiting class component comments

--- frontend/components/tracks/track_index.jsx
+++ frontend/components/tracks/track_index.jsx
@@ -16,23 +16,3 @@ const TrackIndex = (props) => {
 }
 
 export default TrackIndex;
-
-// class TrackIndex extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-
-//     componentDidMount() {
-//         this.props.fetchTracks();
-//     }
-
-//     render() {
-//         return (
-//             <div className="discover-tracks">
-//                 <h1>Discover</h1>
-//                 {this.props.tracks.map((track, idx) => <TrackItemContainer key={track.id} idx={idx} track={track}/>)}
-//                 <div id="sound-wave-everywhere"><img id="sound-wave-everywhere" src="https://d313rqwfqaf3f.cloudfront.net/design/wave_white.svg" width="400" height="400"></img></div>
-//             </div>
-//         )
-//     }
-// }
