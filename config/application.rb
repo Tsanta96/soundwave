14bd19836a7e67c755b4f33d3ef5ca8f97c51cb5
Matteo Santavicca <matteosantavicca@matteos-mbp.lan>
2025-04-18 10:43:07 -0700

    Update spring

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Soundwave
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Adding this line to override the deprecated cache version:
+    config.active_support.cache_format_version = 7.0
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
