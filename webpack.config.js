c95eb0fb51872f9c7125c626b4a7e4cddd8c2724
Matteo Santavicca <matteosantavicca@matteos-mbp.lan>
2025-04-18 19:12:34 -0700

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react'],
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
